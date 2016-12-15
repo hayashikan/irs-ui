3bd8fd9bc4bff1228df37133026a371e86aa1140
Lin Han <jo.jin.lin@gmail.com>
2016-12-15 17:42:23 +0900

    Indent update

--- scripts/controller.js
+++ scripts/controller.js
@@ -42,6 +42,7 @@ angular
 				});};
 		};
 		
+		// alert Generate Done!
 		$scope.alertGenerate = function() {
 			$window.alert('Done! Please click EXPORT then.');
 		}
@@ -106,44 +107,44 @@ angular
     		};
     	}
 	
-	// Day Part Editable
-    	$scope.dayPartEditable = true;
-    	$scope.dayPartOps = 'SAVE';
-    	$scope.toggleDayPart = function () {
-    		$scope.dayPartEditable = !$scope.dayPartEditable;
-    		if ($scope.dayPartEditable) {
-    			$scope.dayPartOps = 'SAVE';
-    		} else {
-    			$scope.dayPartOps = 'EDIT';
-    		};
-    	}
+		// Day Part Editable
+		$scope.dayPartEditable = true;
+		$scope.dayPartOps = 'SAVE';
+		$scope.toggleDayPart = function () {
+			$scope.dayPartEditable = !$scope.dayPartEditable;
+			if ($scope.dayPartEditable) {
+				$scope.dayPartOps = 'SAVE';
+			} else {
+				$scope.dayPartOps = 'EDIT';
+			};
+		}
 
-    	// App/Web Group Spec validation
-    	$scope.validateGroupItems = function(items, index) {
-    		var pattern = /^\d{8}$/i;
-    		var newItem = items[index-1];
-    		validated = pattern.test(newItem);
-    		if (!validated) {
-    			items.splice(index-1, 1);
-    		}
-    		return items
-    		
-    	};
+		// App/Web Group Spec validation
+		$scope.validateGroupItems = function(items, index) {
+			var pattern = /^\d{8}$/i;
+			var newItem = items[index-1];
+			validated = pattern.test(newItem);
+			if (!validated) {
+				items.splice(index-1, 1);
+			}
+			return items
+
+		};
 
 
-    	// Search Demographics
-    	$scope.querySearchDemo = function (query) {
-    		var results = query ? $scope.demographics.filter($scope.createFilterFor(query)) : [];
-    		return results
-    	}
+		// Search Demographics
+		$scope.querySearchDemo = function (query) {
+			var results = query ? $scope.demographics.filter($scope.createFilterFor(query)) : [];
+			return results
+		}
 
-    	$scope.createFilterFor = function (query) {
-    		var lowercaseQuery = angular.lowercase(query);
+		$scope.createFilterFor = function (query) {
+			var lowercaseQuery = angular.lowercase(query);
 
-    		return function filterFn(item) {
-    			return (item._lowername.indexOf(lowercaseQuery)===0) || (item._lowercat.indexOf(lowercaseQuery)===0);
-    		};
-    	};
+			return function filterFn(item) {
+				return (item._lowername.indexOf(lowercaseQuery)===0) || (item._lowercat.indexOf(lowercaseQuery)===0);
+			};
+		};
 		
 		var idIncrementor = 0;
 		//Transform Chip to make Operator duplicatable.
@@ -155,40 +156,39 @@ angular
 			};
 		};
 
-    	// Remove Row: App/Web Group
-    	$scope.removeUnitGroupRow = function (index) {
-    		console.log(index);
-    		$scope.data.UnitOfAnalysis.GroupSpec.splice(index, 1)
-    	};
+		// Remove Row: App/Web Group
+		$scope.removeUnitGroupRow = function (index) {
+			console.log(index);
+			$scope.data.UnitOfAnalysis.GroupSpec.splice(index, 1)
+		};
 
-    	// Add Row: App/Web Group
-    	$scope.addUnitGroupRow = function () {
-    		$scope.data.UnitOfAnalysis.GroupSpec.push({GroupName: "", GroupItems: []});
-    	}
+		// Add Row: App/Web Group
+		$scope.addUnitGroupRow = function () {
+			$scope.data.UnitOfAnalysis.GroupSpec.push({GroupName: "", GroupItems: []});
+		}
 
-    	// Remove Row: Target Group
-    	$scope.removeTargetGroupRow = function (index) {
-    		console.log(index);
-    		$scope.data.ReportType.TargetGroup.splice(index, 1)
-    	};
+		// Remove Row: Target Group
+		$scope.removeTargetGroupRow = function (index) {
+			console.log(index);
+			$scope.data.ReportType.TargetGroup.splice(index, 1)
+		};
 
-    	// Add Row: Target Group
-    	$scope.addTargetGroupRow = function () {
-    		$scope.data.ReportType.TargetGroup.push({GroupName: "", QueryArray: [], Query: ""});
-    	}
+		// Add Row: Target Group
+		$scope.addTargetGroupRow = function () {
+			$scope.data.ReportType.TargetGroup.push({GroupName: "", QueryArray: [], Query: ""});
+		}
 
-    	// Target Group: Join Group Query
-    	$scope.join2Query = function (queryarray) {
-    		//return queryarray.join(' ');
-    		var results = queryarray.map(function (item){
-    			return item.query;
-    		});
-    		return results.join(' ');
+		// Target Group: Join Group Query
+		$scope.join2Query = function (queryarray) {
+			//return queryarray.join(' ');
+			var results = queryarray.map(function (item){
+				return item.query;
+			});
+			return results.join(' ');
+		}
 
-    	}
-
-    	function loadDemographics() {
-    		var demographics = [{
+		function loadDemographics() {
+			var demographics = [{
 					name: 'and', cat: 'Operator', query: "&"
 				},{
 					name: 'or', cat: 'Operator', query: "|"
@@ -209,7 +209,6 @@ angular
 				},{
 					name: '50 - 64', cat: 'Age Group', query: "(Age_Group=='08' | Age_Group=='09' | Age_Group=='10')"
 				}];
-
 			return demographics.map(function (demo) {
 				demo._lowername = demo.name.toLowerCase();
 				demo._lowercat = demo.cat.toLowerCase();
@@ -217,7 +216,7 @@ angular
 			});
     	};
 
-    	$scope.frequencyTypes = [
+		$scope.frequencyTypes = [
 			'Daily',
 			'Daily Average',
 			'Monthly'
